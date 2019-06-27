fb32816e4c5f70d366652f2ead58048884146b11
jsjrjz08 <jsjrjz08@126.com>
2019-06-27 18:34:21 +0800

    leetcode-203实现。包含带有虚拟头结点的实现、不带虚拟头结点的实现、本地调试的实现

---
+++ 04-LinkedList-base/src/Solution.java
@@ -0,0 +1,121 @@
+
+
+//leetcode 203
+public class Solution {
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return "val="+val+",next="+next;
+        }
+    }
+
+    /**
+     * 删除链表中值为指定值的元素we
+     * @param head 链表的头结点指针
+     * @param val 指定值
+     * @return 返回删除指定元素后的链表
+     */
+    //使用虚拟头结点,可以统一逻辑，不必处理特殊情况
+    public ListNode removeElements(ListNode head, int val) {
+
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+
+        ListNode prev = dummyHead;
+        while(prev.next != null) {
+            if(prev.next.val == val) {
+                ListNode delNode = prev.next;
+                prev.next = prev.next.next;
+                delNode.next = null;
+                //删除后，prev不移动!!!
+            } else {
+                prev = prev.next;
+            }
+        }
+//        return head;//注意返回值！！！！
+        return dummyHead.next;
+    }
+
+    /**
+     * 删除链表中值为指定值的元素we
+     * @param head 链表的头结点指针
+     * @param val 指定值
+     * @return 返回删除指定元素后的链表
+     */
+    //不设计头结点
+    public ListNode removeElements_1(ListNode head, int val) {
+
+        while(head != null && head.val == val) {
+            ListNode delNode = head;
+            head = head.next;
+            delNode.next = null;
+        }
+
+        //空链表
+        if(head == null) {
+            return head;
+        }
+
+        ListNode prev = head;
+        while(prev.next != null) {
+            if(prev.next.val == val) {
+                ListNode delNode = prev.next;
+                prev.next = prev.next.next;
+                delNode.next = null;
+                //删除后，prev不移动!!!
+            } else {
+                prev = prev.next;
+            }
+        }
+        return head;
+    }
+
+    /**
+     * 删除链表中值为指定值的元素we
+     * @param head 链表的头结点指针
+     * @param val 指定值
+     * @return 返回删除指定元素后的链表
+     */
+    //不设计头结点
+    public ListNode removeElements_0(ListNode head, int val) {
+        //1)空链表
+        if(head == null) {
+            return null;
+        } else {
+
+            while(head != null && head.val == val) {
+                ListNode delNode = head;
+                head = head.next;
+                delNode.next = null;
+            }
+
+            //2)空链表
+            //1)与2)可以合并
+            if(head == null) {
+                return head;
+            }
+
+            ListNode prev = head;
+            while(prev.next != null) {
+                if(prev.next.val == val) {
+                    ListNode delNode = prev.next;
+                    prev.next = prev.next.next;
+                    delNode.next = null;
+                    //删除后，prev不移动!!!
+                } else {
+                    prev = prev.next;
+                }
+            }
+            return head;
+        }
+    }
+
+}
+
---
+++ 04-LinkedList-base/src/Solution2.java
@@ -0,0 +1,81 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    //添加构造方法
+    ListNode(int[] arrNums) {
+        if(arrNums == null || arrNums.length == 0) {
+            throw new IllegalArgumentException("arr is empty");
+        }
+
+        this.val = arrNums[0];//
+        ListNode cur = this;//
+        for(int i=1; i<arrNums.length; i++) {
+            cur.next = new ListNode(arrNums[i]);
+            cur = cur.next;
+        }
+    }
+
+    //以head为头结点的链表信息
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("elements [");
+
+        ListNode cur = this;//
+        while (cur != null) {
+            sb.append(cur.val+"->");
+            cur = cur.next;
+        }
+
+        sb.append("NULL]");
+        return sb.toString();
+    }
+}
+
+//本地调试，可以自己制作测试用例、debug等。修改ListNode的构造方法、添加toString方法
+//leetcode 203
+public class Solution2 {
+
+    /**
+     * 删除链表中值为指定值的元素we
+     * @param head 链表的头结点指针
+     * @param val 指定值
+     * @return 返回删除指定元素后的链表
+     */
+    //使用虚拟头结点,可以统一逻辑，不必处理特殊情况
+    public ListNode removeElements(ListNode head, int val) {
+
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+
+        ListNode prev = dummyHead;
+        while(prev.next != null) {
+            if(prev.next.val == val) {
+                ListNode delNode = prev.next;
+                prev.next = prev.next.next;
+                delNode.next = null;
+                //删除后，prev不移动!!!
+            } else {
+                prev = prev.next;
+            }
+        }
+//        return head;//注意返回值！！！！
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {//不能提交leetcode，因为leetcode上ListNode的构造方法是不存在的，不能通过编译!
+        int[] arr = {6,6,2,1,5,6,3,6,7};
+        ListNode head = new ListNode(arr);
+        System.out.println(head);
+
+        System.out.println((new Solution2()).removeElements(head,6));
+
+    }
+
+}
+
