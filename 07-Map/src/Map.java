d048dc85036d200bcc43c98366f62df4f24bb38a
jsjrjz08 <jsjrjz08@126.com>
2019-07-10 15:36:32 +0800

    添加注释

--- 07-Map/src/Map.java
+++ 07-Map/src/Map.java
@@ -1,9 +1,24 @@
+/**
+ * Map不存储key重复的键值对
+ * @param <K>
+ * @param <V>
+ */
 public interface Map<K,V> {
     int getSize();
     boolean isEmpty();
+
+    //当key存在时，返回true；当key不存在时，返回false
     boolean contains(K key);
+
+    //当key存在时，返回value；当key不存在时，返回null
     V get(K key);
+
+    //当key存在时，更新value；当key不存在时，抛异常
     void set(K key,V value);
+
+    //当key存在时，更新value；当key不存在时，添加键值对
     void add(K key,V value);
+
+    //当key存在时，删除key，并返回value；当key不存在时，返回null
     V remove(K key);
 }
